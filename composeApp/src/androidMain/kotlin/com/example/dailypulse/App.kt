a8842e360df5ff89f79f48eb6f732e669b5e1f14
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-03-04 17:20:03 +0530

    Added dark and light mode in app

--- composeApp/src/androidMain/kotlin/com/example/dailypulse/App.kt
+++ composeApp/src/androidMain/kotlin/com/example/dailypulse/App.kt
@@ -1,17 +1,28 @@
 package com.example.dailypulse
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import com.example.dailypulse.articles.ArticlesViewModel
-import com.example.dailypulse.screens.ArticlesScreen
 
 @Composable
 fun App(articlesViewModel: ArticlesViewModel) {
-    MaterialTheme {
+    val darkTheme = isSystemInDarkTheme()
+
+    val colorScheme = if (darkTheme) {
+        darkColorScheme()
+    } else {
+        lightColorScheme()
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme
+    ) {
         Surface(
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
